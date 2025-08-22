f89ece4851cba5f919c6193929108b84937277ce
Raska Gábor <raskagabor22@gmail.com>
2025-08-22 12:04:32 +0200

    opening hours

--- inc/Base/BajaPublicInformationCustomPostType.php
+++ inc/Base/BajaPublicInformationCustomPostType.php
@@ -582,7 +582,7 @@ class BajaPublicInformationCustomPostType extends BajaPublicInformationBaseContr
                     foreach ($days as $key => $label) {
                         $from = isset($opening_hours[$key]['from']) ? $opening_hours[$key]['from'] : '';
                         $to   = isset($opening_hours[$key]['to']) ? $opening_hours[$key]['to'] : '';
-                        $time = ($from || $to) ? $from . ' - ' . $to : '-';
+                        $time = ($from || $to) ? $from . ' - ' . $to : 'ZÁRVA';
                         echo '<tr><td>' . esc_html($label) . '</td><td>' . esc_html($time) . '</td></tr>';
                     }
                     echo '</table>';
